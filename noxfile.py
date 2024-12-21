6301707ea1686347cd1af3e8ae10b0b893d392c2
meteoDaniel <daniel.lassahn@alitiq.com>
2024-12-22 00:51:29 +0100

    Initial commit of alitiq's rest API sdk

---
+++ noxfile.py
@@ -0,0 +1,42 @@
+""" nox session definition """
+import os
+from typing import Any
+import subprocess
+
+import nox
+
+locations = "alitiq",
+
+
+# https://cjolowicz.github.io/posts/hypermodern-python-02-testing/
+# https://cjolowicz.github.io/posts/hypermodern-python-03-linting/
+
+
+@nox.session(python=None)
+def black(session):
+    """runs black to do some automatic code automatisation"""
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+@nox.session(python=None)
+def lint(session):
+    """runs the linter to inspect the code"""
+    args = session.posargs or locations
+    session.install(
+        "flake8",
+        # "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-docstrings"
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python=None)
+def isort(session: Any) -> None:
+    """runs isort to sort the imports"""
+    args = session.posargs or locations
+    session.install("isort")
+    session.run("isort", *args)
